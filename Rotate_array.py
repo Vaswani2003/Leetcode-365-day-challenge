0479d22d1a1f01df6325c68f96a1447bfb53f726
Vinamra Vaswani <100992956+Vaswani2003@users.noreply.github.com>
2024-04-12 15:56:58 +0530

    Create Rotate_array.py

---
+++ Rotate_array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = k%len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k%len(nums)
+
+        while(k>0):
+            nums.insert(0, nums.pop())
+            k -= 1
